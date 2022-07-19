c70b04ca7d62e2c0dc464dbff33f24bfaf3c0e28
javad <javadr@gmail.com>
2022-07-19 15:12:45 +0430

    plot a line for the best result

--- code/utils.py
+++ code/utils.py
@@ -14,18 +14,23 @@ def res_plot(data, desc='', p=3):
     for idx, t in enumerate(['loss', 'f1']):
         x = range(1,epochs+1)
         ax = axes[idx]
-        axes[idx].set_xticks(x[::p])
+        ax.set_xticks(x[::p])
+        ax.set_xticklabels(ax.get_xticks(), rotation = 90)
         # title = 'Loss' if idx==0 else 'F1 Score'
         # ax.set_title(title)
         ax.set_xlabel('epoch')
-        ax.plot(x, data[f'train_{t}'], '-o', label=legend[0])
-        ax.plot(x, data[f'val_{t}'], '-o', label=legend[1])
+        ax.plot(x, data[f'train_{t}'], '-', label=legend[0])
+        ax.plot(x, data[f'val_{t}'], '-', label=legend[1])
         c = 0
         for i,j in list(zip( [id for tup in [(i,i) for i in x[::p][:-1]] for id in tup],
                     [item for tup in zip(data[f'train_{t}'][::p][:-1],data[f'val_{t}'][::p][:-1]) for item in tup] )):
-            ax.annotate(f"{j:.{p}f}", xy=(i,j), rotation=45, va='bottom', color=['g', 'k'][c:=(c+1)%2])
+            c = 1-c
+            ax.annotate(f"{j:.3f}", xy=(i,j), rotation=60, va=['top','bottom'][c] if idx else ['bottom','top'][c], color=['g', 'k'][c])
         ax.annotate(f"{data[f'train_{t}'][-1]:.{max(4,p)}f}", xy=(epochs,data[f'train_{t}'][-1]), color='r')
         ax.annotate(f"{data[f'val_{t}'][-1]:.{max(4,p)}f}", xy=(epochs,data[f'val_{t}'][-1]), color='r')
+        notch = (max if idx else min)(data[f'val_{t}'])
+        ax.plot([notch, len(data[f'val_{t}'])], [notch, notch], 'gray')
+        ax.annotate(f"{notch:.4f}", xy=(1,notch), ha='right', c='black')
         ax.legend()
     metric_name = f'../images/plot[{datetime.now().strftime("%y%m%d%H%M")}]-Ep{epochs}B{CFG.batch_size}{desc}.png'
     fig.suptitle(desc, fontsize=16)
